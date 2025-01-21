0f7784e0670011a6a634b21e32aaaa6532b9eb37
CETECOM <CETECOM@AVAWSV01LC0606.duoc.acad>
2025-01-21 16:09:33 -0300

    Servicio autenticacion, Local storage

--- src/app/Servicios/auth.service.ts
+++ src/app/Servicios/auth.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
+import { LocalStorageService } from './local-storage.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   private static isLogged: boolean = false;
+  private storage: LocalStorageService = new LocalStorageService();
   constructor() {}
 
   login(user: string, pass: string): boolean {
@@ -19,11 +21,49 @@ export class AuthService {
     }
   }
 
+  loginStorage(user:string, pass: string):boolean{
+    const listaUsuarios = this.storage.getItem("users") || []
+
+    const conectado = listaUsuarios.find(
+      (userFind:any) => (userFind.username==user || userFind.correo==user)&&
+    userFind.pass==pass
+  );
+
+  if(conectado){
+    this.storage.setItem('Conectado',conectado);
+    return true;
+  }else{
+    return false;
+  }
+  }
+
+  Registrar(user:string,correo:string,pass:string){
+
+    const listaUsuarios = this.storage.getItem('users') || []
+
+    if(listaUsuarios.find((userFind: any) => userFind.username==user || userFind.correo==correo)){
+    } {
+      return false;
+    }
+
+    const nuevoUsuario = {
+      id: listaUsuarios.lenght+1,
+      username: user,
+      correo: correo,
+      pass: pass
+    };
+
+    listaUsuarios.push(nuevoUsuario);
+    this.storage.setItem('users', listaUsuarios);
+    return true;
+  }
+
+
   isConnected(): boolean {
-    return AuthService.isLogged;
+    return this.storage.getItem('conectado') !== null;
   }
 
   logout() {
-    AuthService.isLogged = false;
+    this.storage.removeItem('conectado');
   }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -43,6 +43,7 @@ export class HomePage {
     }
   }
 
+  
   ngAfterContentInit() {
     this.animacionLogin();
   }
